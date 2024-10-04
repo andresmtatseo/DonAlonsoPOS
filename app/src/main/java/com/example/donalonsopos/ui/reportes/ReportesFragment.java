1d317d06170dac851a8ae48cb68c906a3219b60e
Roberto Stuch <robertostuch22@gmail.com>
2024-10-04 19:32:10 -0400

    Cambio xs

--- app/src/main/java/com/example/donalonsopos/ui/reportes/ReportesFragment.java
+++ app/src/main/java/com/example/donalonsopos/ui/reportes/ReportesFragment.java
@@ -19,11 +19,11 @@ public class ReportesFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM1 = "paramm1";
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
+    private String mParamm1;
     private String mParam2;
 
     public ReportesFragment() {
@@ -34,15 +34,15 @@ public class ReportesFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
+     * @param paramm1 Parameter 1.
      * @param param2 Parameter 2.
      * @return A new instance of fragment ReportesFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ReportesFragment newInstance(String param1, String param2) {
+    public static ReportesFragment newInstance(String paramm1, String param2) {
         ReportesFragment fragment = new ReportesFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM1, paramm1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
@@ -52,7 +52,7 @@ public class ReportesFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParamm1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
