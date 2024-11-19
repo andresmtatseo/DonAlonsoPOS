94948f7332e828b3553a062612a7239b3ede9359
Roberto Stuch <robertostuch22@gmail.com>
2024-11-19 00:40:44 -0400

    Spinner y boton

--- app/src/main/java/com/example/donalonsopos/ui/reportes/ReportesFragment.java
+++ app/src/main/java/com/example/donalonsopos/ui/reportes/ReportesFragment.java
@@ -2,65 +2,27 @@ package com.example.donalonsopos.ui.reportes;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.donalonsopos.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ReportesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ReportesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "paramm1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParamm1;
-    private String mParam2;
-
-    public ReportesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param paramm1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ReportesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ReportesFragment newInstance(String paramm1, String param2) {
-        ReportesFragment fragment = new ReportesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, paramm1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class ReportesFragment extends AppCompatActivity {
+    Spinner seleccion;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParamm1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+        setContentView(R.layout.fragment_reportes);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reportes, container, false);
-    }
+
+        seleccion = findViewById(R.id.spinner_r); // Configurar el Spinner con opciones
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.spinner_reportes, android.R.layout.simple_spinner_item);
+
+        seleccion.setAdapter(adapter);
+        }
 }
